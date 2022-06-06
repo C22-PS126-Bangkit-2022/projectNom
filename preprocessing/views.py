23e27cb08d4c8853ea1a61c07f7e29c6b504c933
fujimotoAr <theofilus.arkhi@mail.ugm.ac.id>
2022-06-06 15:52:14 +0700

    preprocessing fixed

--- preprocessing/views.py
+++ preprocessing/views.py
@@ -5,6 +5,7 @@ from PIL import Image
 import json
 import base64
 import io
+
 # Create your views here.
 
 TARGET_SHAPE = (299,299)
@@ -15,16 +16,15 @@ def decodeImage(img):
     return decoded_image
 
 def encodeImage(img):
-    decoded_image = bytes(str(img),"utf-8")
-    encoded_image = base64.b64encode(decoded_image)
+    encoded_image=base64.b64encode(img)
     return encoded_image
 
 def preprocessImage(request):
     data = json.loads(request.body.decode('utf-8'))
     decoded_image = decodeImage(data["img"])
     decoded_image = Image.open(io.BytesIO(decoded_image)).resize(TARGET_SHAPE,Image.LANCZOS).convert("RGB")
-    encoded_image = encodeImage(decoded_image)#.decode('utf-8')
-
-    print(encoded_image)
+    buffer = io.BytesIO()
+    decoded_image.save(buffer,format="JPEG")
+    encoded_image=encodeImage(buffer.getvalue()).decode('utf-8')
     data['img'] = encoded_image
     return JsonResponse(data,safe=False)
