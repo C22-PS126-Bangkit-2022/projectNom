78812e38f8f0b6a609359797035509ae51b59495
RaidRafif <raid.rafif3@gmail.com>
2022-06-11 23:42:35 +0700

    Add predict_json and IAM key.

--- preprocessing/views.py
+++ preprocessing/views.py
@@ -7,7 +7,15 @@ import base64
 import io
 import requests
 import tensorflow as tf
+
+# Google Cloud Library
+import googleapiclient.discovery
+from google.api_core.client_options import ClientOptions
 # Create your views here.
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'project-nom-351311-a6e5d5ac5601.json'
+PROJECT = "project-nom-351311"
+REGION = "asia-southeast1"
 
 TARGET_SHAPE = (299, 299)
 MODEL = tf.keras.model.load_model(
@@ -108,12 +116,71 @@ def encodeImage(img):
     return encoded_image
 
 
+def predict_json(project, region, model, instances, version=None):
+    """Send json data to a deployed model for prediction.
+
+    Args:
+        project (str): project where the Cloud ML Engine Model is deployed.
+        region (str): regional endpoint to use; set to None for ml.googleapis.com
+        model (str): model name.
+        instances ([Mapping[str: Any]]): Keys should be the names of Tensors
+            your deployed model expects as inputs. Values should be datatypes
+            convertible to Tensors, or (potentially nested) lists of datatypes
+            convertible to tensors.
+        version: str, version of the model to target.
+    Returns:
+        Mapping[str: any]: dictionary of prediction results defined by the
+            model.
+    """
+    # Create the ML Engine service object.
+    # To authenticate set the environment variable
+    # GOOGLE_APPLICATION_CREDENTIALS=<path_to_service_account_file>
+    prefix = "{}-ml".format(region) if region else "ml"
+    api_endpoint = "https://{}.googleapis.com".format(prefix)
+    client_options = ClientOptions(api_endpoint=api_endpoint)
+
+    service = googleapiclient.discovery.build(
+        'ml', 'v1', client_options=client_options, cache_discovery=False)  # cache_discovery needed to bypass oauth2client unavailable error
+    name = 'projects/{}/models/{}'.format(project, model)
+
+    if version is not None:
+        name += '/versions/{}'.format(version)
+
+    instances_list = instances.tolist()
+    response = service.projects().predict(
+        name=name,
+        body={'instances': instances_list}
+    ).execute()
+
+    input_data_json = {'instances': instances_list}
+
+    # print(
+    #     len(json.dumps(input_data_json))
+    # )  # Get the size of json object
+
+    response = service.projects().predict(
+        name=name,
+        body=input_data_json
+    ).execute()
+
+    if 'error' in response:
+        raise RuntimeError(response['error'])
+
+    return response['predictions']
+
+
 def runInference(image, model, label_list):
     # TODO: Function to call cloud endpoint
     # ...
 
     # Create dict key "label" to store the prediction result
-    result = model.predict(image)
+    result = predict_json(
+        project=PROJECT,
+        region=REGION,
+        model=model,
+        instances=image
+    )
+    # result = model.predict(image)
 
     # Get the result
     result_class = label_list[np.argmax(result)]
